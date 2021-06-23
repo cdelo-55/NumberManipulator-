112e02676ebe8109f750de14d2ba7cc199b6e891
Delo <34003@nelnet.net>
2021-06-23 10:00:24 -0500

    number manipulator

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NumberManipulator_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to be manipulated:");
+            
+            int input = Convert.ToInt32(Console.ReadLine());
+            input = input * input;
+            input += 14;
+            input -= 3;   
+            Console.WriteLine(input);
+        }
+    }
+}
